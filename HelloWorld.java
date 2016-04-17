d687d37eb84227fac2f67f681ba2c31d100d275d
Colton P. van Assche <coltonvanassche@colton-mac.local>
2016-04-17 17:31:28 -0400

    Java file of famous program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Class HelloWorld prints out famous line.
+// Part of Intro to Java Programming
+
+public class HelloWorld {
+	public static void main(String[] args) {
+	    System.out.println("Hello, world!"); //subroutine call statement output
+	}
+}
+
+// Compiled into HelloWorld.class
